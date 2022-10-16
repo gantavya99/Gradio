6126affbe492f6bfe717db88ef62d6f75475f85d
Gantavya Saraswat <gantavya99@gmail.com>
2022-10-16 18:43:57 +0530

    Added Discover Header

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,6 +1,16 @@
 import { Error,Loader,SongCard } from "../components";
-import {genres}
+import {genres} from '../assets/constants';
 const Discover = () => 
-<div>Discover</div>;
+{
+    console.log(genres);
+    return(
+    <div className="flex flex-col">
+        <div className=".w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        Discover
+        </div>
+        </div>
+    )
+}
+
 
 export default Discover;
