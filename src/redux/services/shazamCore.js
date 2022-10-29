f1f704d3334cab5a2827c8284151a29cf0dca6af
Gantavya Saraswat <gantavya99@gmail.com>
2022-10-29 19:05:08 +0530

    Feat:Shazam API

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,17 +1,19 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '06b58b70bemsh79036c742fc3282p11015fjsn42e869176c14',
-      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-    }
-  };
-  
-  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 
 export const shazamCoreApi=createApi({
     reducerPath:'shazamCoreApi',
+    baseQuery:fetchBaseQuery({
+        baseUrl:'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders:(headers)=>{
+            headers.set('X-RapidAPI-Key','06b58b70bemsh79036c742fc3282p11015fjsn42e869176c14')
+            return headers;
+        }
+    }),
+    endpoints:(builder)=>({
+        getTopCharts:builder.query({query:()=>'/charts/world'}),
+    })
 });
+
+export const{
+    useGetTopChartsQuery
+}=shazamCoreApi;
