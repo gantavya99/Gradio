25fb91f6836b8a9c2f642de8d546d788444af16d
Gantavya Saraswat <gantavya99@gmail.com>
2022-11-05 22:15:20 +0530

    Created Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <>
-    <h1>Login Page</h1>
-    </>
+    <div className="bg">
+        <div className="container">
+            <h1>Login</h1>
+            <h2>Hey, Enter your details below</h2>
+            <div>
+            <input className="email-pass" type="email" placeholder="Enter your Email"/>
+            </div>
+            <div>
+            <input type="password" className="email-pass" placeholder="Password"/>
+            </div>
+            <div>
+                Forgot Password? <a hre="/">Click here !</a>
+            </div>
+            <div>
+                <button className="login-btn">Login</button>
+            </div>
+            <div>
+                <p>-Or Sign in with-</p>
+            </div>
+            <div>
+                <span>
+                    <button className="">Google</button>
+                </span>
+                <span>
+                    <button>Apple ID</button>
+                </span>
+                <span>
+                    <button>Facebook</button>
+                </span>
+            </div>
+            <div>
+                <span>Dont have an account? </span>
+                <a href="/">Sign up</a>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
