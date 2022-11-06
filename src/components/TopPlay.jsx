005f73d2dc1f0b845f64f2789df06547a93d86c4
Gantavya Saraswat <gantavya99@gmail.com>
2022-11-06 18:55:20 +0530

    FIxed Gradio Logo

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,3 +1,13 @@
+import {iseEffect,useRef} from 'react';
+import {link} from 'react-router-dom';
+import {useSelector,useDispatch} from react-redux;
+import {Swiper,SwiperSlide} from 'swiper/react';
+import {FreeMode} from 'swiper';
+import PlayPause from './PlayPause';
+import {PlayPause,setActiveSong} from '../redux/features/playerSlice';
+import {useGetTopChartsQuery} from '../redux/services/shazamCore'; 
+import 'swiper/css';
+import 'swiper/css/free-mode';
 const TopPlay = () => (
   <div>TopPlay</div>
 );
